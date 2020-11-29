31ad0064e81736ca386210480e4409d9e833fd4c
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2020-11-29 18:42:20 +0100

    research.html créée et base.html

--- platform_project/food/urls.py
+++ platform_project/food/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^research/$', views.research, name="research"),
     # url(r'^$', views.index), # "/store" will call the method "index" in "views.py"
 ]
--- platform_project/food/views.py
+++ platform_project/food/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'food/index.html')
 
+
+def research(request):
+    return render(request, 'food/research.html')
+
