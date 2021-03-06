c3dc1284434f13ffa7b459833edcb2de3286e31a
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2021-03-06 10:55:22 +0100

    page d'intenfication créée

--- platform_nutella/accounts/form.py
+++ platform_nutella/accounts/form.py
@@ -2,4 +2,12 @@ from django import forms
 
 
 class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+    your_name = forms.CharField(label='user name', max_length=100, required=False)
+    email = forms.EmailField(label='email', max_length=100, required=False)
+    password = forms.CharField(label='password', max_length=100, required=False)
+    password2 = forms.CharField(label='re-write password', max_length=100, required=False)
+
+
+class FormLogin(forms.Form):
+    user_name = forms.CharField(label="Nom d'utilisateur", max_length=100)
+    password = forms.CharField(label='Mot de passe', max_length=100)
--- platform_nutella/accounts/views.py
+++ platform_nutella/accounts/views.py
@@ -1,27 +1,50 @@
-from django.shortcuts import render
-
-from django.http import HttpResponseRedirect
 from django.http import HttpResponse
+from django.shortcuts import render
+from .form import NameForm, FormLogin
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
-from .form import NameForm
+
+def login_page(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = FormLogin(request.POST)
+        user_name = request.POST.get("user_name")
+        password = request.POST.get("password")
+        # check whether it's valid
+        user = authenticate(request, username=user_name, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return render(request, 'accounts/connexion_page.html')
+        else:
+            # Return an 'invalid login' error message.
+            return HttpResponse('login invalid')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = FormLogin()
+
+    return render(request, 'accounts/login.html', {'form': form})
 
 
-def login(request):
-    return render(request, 'accounts/login.html')
+# def register(request):
+    # return render(request, 'accounts/register.html')
 
 
 def register(request):
-    return render(request, 'accounts/register.html')
-
-
-def get_name(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = NameForm(request.POST)
-        print(form)
+        name = request.POST.get("your_name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
         # check whether it's valid:
-        if form.is_valid():
+        if form.is_valid() and password == password2:
+            User.objects.create_user(name, email, password)
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
@@ -31,4 +54,4 @@ def get_name(request):
     else:
         form = NameForm()
 
-    return render(request, 'accounts/name.html', {'form': form})
+    return render(request, 'accounts/register.html', {'form': form})
