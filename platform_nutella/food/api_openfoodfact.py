2a5767b54ba2b046e934dac22111a45e56193b23
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2021-04-04 13:02:47 +0200

    repère nutritionnel récupéré d'openfoodfact

--- platform_nutella/food/api_openfoodfact.py
+++ platform_nutella/food/api_openfoodfact.py
@@ -14,6 +14,11 @@ INFO1 = "product_name_fr"
 INFO2 = "image_front_thumb_url"
 INFO3 = "nutrition_grade_fr"
 INFO4 = "url"
+NUTRIMENTS = "nutriments"
+FAT = "fat"
+SATURATED_FAT = "saturated-fat"
+SUGAR = "sugars"
+SALT = "salt"
 
 
 class DataApi:
@@ -52,12 +57,14 @@ class DataApi:
             'json': 'true',
         }
         response = requests.get(self.url, params=payload_substitutes).json()
-        data = response["products"][0]
-        # with open("data_file.json", "w") as write_file:
-            # json.dump(data, write_file, indent=4)
+        data = response["products"]
+        with open("data_file.json", "w") as write_file:
+            json.dump(data, write_file, indent=4)
         return data
 
-    def select_key_test(self, key1=INFO1, key2=INFO2, key3=INFO3, key4=INFO4):
+    def select_key_test(self, key1=INFO1, key2=INFO2, key3=INFO3, key4=INFO4,
+                        key5=NUTRIMENTS, key6=FAT, key7=SATURATED_FAT,
+                        key8=SUGAR, key9=SALT):
         """The different keys are sorted and placed in lists"""
         list_general = []
         categories = self.get_categories_name_food()
@@ -65,7 +72,10 @@ class DataApi:
 
         data = self.get_data_products_category(category)
         for item in data:
-            product_list = [item.get(key1), item.get(key2), item.get(key3), item.get(key4)]
+            product_list = [item.get(key1), item.get(key2), item.get(key3),
+                            item.get(key4), item.get(key5).get(key6),
+                            item.get(key5).get(key7), item.get(key5).get(key8),
+                            item.get(key5).get(key9)]
             if '' not in product_list and None not in product_list:
                 list_general.append(product_list)
         return list_general
