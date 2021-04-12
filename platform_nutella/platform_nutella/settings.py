ffac48736cc1645d3b56c7b19066933fbb220eff
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2021-04-12 10:38:26 +0200

    setting

--- platform_nutella/platform_nutella/settings.py
+++ platform_nutella/platform_nutella/settings.py
@@ -158,11 +158,11 @@ if os.environ.get('ENV') == 'PRODUCTION':
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
     # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),  # changement 'static' en '/static/'
+        os.path.join(PROJECT_ROOT, 'static'),  # changement 'static' en '/static/'
     )
 
     # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
