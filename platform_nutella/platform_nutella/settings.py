2f73b42e01c17b7a8e543bb6fdd0e1e41c4cac8f
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2021-04-12 17:23:06 +0200

    setting

--- platform_nutella/platform_nutella/settings.py
+++ platform_nutella/platform_nutella/settings.py
@@ -148,7 +148,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_URL = '/staticfiles/'
 STATIC_URL = '/static/'
 
 INTERNAL_IPS = ['127.0.0.1']
@@ -165,8 +164,8 @@ if os.environ.get('ENV') == 'PRODUCTION':
         os.path.join(PROJECT_ROOT, 'static'),
     )
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
