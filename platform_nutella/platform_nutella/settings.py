c343e3f87284ec731b4e38994335e490d63afc93
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2021-04-12 17:18:16 +0200

    setting

--- platform_nutella/platform_nutella/settings.py
+++ platform_nutella/platform_nutella/settings.py
@@ -148,8 +148,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-# STATIC_URL = '/static/'
+# STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 INTERNAL_IPS = ['127.0.0.1']
 
